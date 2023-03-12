03c45f539b62786a82c3f94e1acf3a9e9cf4325c
George Leyva <leyvageorge@hotmail.com>
2023-03-12 11:02:12 -0700

    my commit to add controllers

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ const app = express()
 const PORT = process.env.PORT
 console.log(PORT)
 
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+const breadsController = require('./controllers/breads_controllers')
+app.use('/breads',breadsController)
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT: ', PORT)
+})
